3134b26d14306b4852c12fe7728ef56d953fab2e
Daniel Maxime <root@maxux.net>
2014-10-03 18:01:17 +0200

    update tinymce code

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ $(document).ready(function() {
 	$('.thumb a').tooltip();
 	
 	tinymce.init({
-		selector: "textarea",
+		selector: "textarea.forumtext",
 		plugins : "bbcode",
-		theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,styleselect,removeformat,cleanup,code",
+		// theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,styleselect,removeformat,cleanup,code",
+		theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,removeformat,cleanup,code",
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
-		theme_advanced_styles : "Code=codeStyle;Citer=quoteStyle",
+		// theme_advanced_styles : "Code=code;Citer=cite",
 		entity_encoding: "raw",
 		add_unload_trigger: false,
 		remove_linebreaks: false,
