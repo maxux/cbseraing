94c40337dfbb49c4ba7e82378f9fdd27227ca3d7
Daniel Maxime <root@maxux.net>
2014-10-03 16:56:28 +0200

    add tinymce editor

--- js/script.js
+++ js/script.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
 		return false;
 	});
 	$('.thumb a').tooltip();
+	
+	tinymce.init({
+		selector: "textarea",
+		plugins : "bbcode",
+		theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,styleselect,removeformat,cleanup,code",
+		theme_advanced_buttons2 : "",
+		theme_advanced_buttons3 : "",
+		theme_advanced_styles : "Code=codeStyle;Citer=quoteStyle",
+		entity_encoding: "raw",
+		add_unload_trigger: false,
+		remove_linebreaks: false,
+		inline_styles: false,
+		convert_fonts_to_spans: false,
+		theme_advanced_path: false,
+	});
 });
