abcd4430796d8fb7ffeaa6e7dadba3573b44db69
Daniel Maxime <root@maxux.net>
2014-10-11 03:06:11 +0200

    removing tinymce from source tree

--- js/script.js
+++ js/script.js
@@ -7,24 +7,6 @@ $(document).ready(function() {
 		return false;
 	});
 	$('.thumb a').tooltip();
-	
-	/*
-	tinymce.init({
-		selector: "textarea.forumtext",
-		plugins : "bbcode",
-		// theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,styleselect,removeformat,cleanup,code",
-		theme_advanced_buttons1 : "bold,italic,underline,undo,redo,image,removeformat,cleanup,code",
-		theme_advanced_buttons2 : "",
-		theme_advanced_buttons3 : "",
-		// theme_advanced_styles : "Code=code;Citer=cite",
-		entity_encoding: "raw",
-		add_unload_trigger: false,
-		remove_linebreaks: false,
-		inline_styles: false,
-		convert_fonts_to_spans: false,
-		theme_advanced_path: false,
-	});
-	*/
 });
 
 function quote(id) {
